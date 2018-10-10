4e392a0aabbb0bb4087165626f8f248a60dc71e2
Gaëtan <gaetan.lefevre26@gmail.com>
2018-10-10 18:44:08 +0200

    Initial commit

---
+++ td1/exo1.py
@@ -0,0 +1,18 @@
+import pprint
+
+import pymongo as mongo
+
+mongoConnexion = mongo.MongoClient("localhost", 27017)
+
+coll = mongoConnexion.get_database('BigDataTD').get_collection('Patients').find()
+coll2 = mongoConnexion.get_database('BigDataTD').get_collection('PatientsCopie')
+
+valeur = 0
+
+for doc in coll:
+    #pprint.pprint(doc)
+    #print(doc['age'])
+    valeur += doc['age']
+    coll2.insert_one(doc)
+
+print('Moyenne age :', valeur/coll.count(True))
